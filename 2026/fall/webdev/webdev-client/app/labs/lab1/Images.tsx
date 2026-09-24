655c6147bbbb7e4f8a9e3781b577c5bc501d5745
pricefy-dotcom <tylerprice.it@gmail.com>
2026-09-23 20:55:20 -0400

    Section 1.3.5 Image Tag

---
+++ 2026/fall/webdev/webdev-client/app/labs/lab1/Images.tsx
@@ -0,0 +1,33 @@
+export default function Images() {
+  return (
+    <div id="wd-images">
+      <h4>Image tag</h4>
+      Loading an image from the internet:
+      <br />
+      <img
+        id="wd-starship"
+        width="400px"
+        alt="Starship"
+        src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+      />
+      <br />
+      Loading a local image:
+      <br />
+      <img
+        id="wd-teslabot"
+        src="/images/teslabot.jpg"
+        height="200px"
+        alt="Tesla Bot (Optimus) humanoid robot"
+      />
+      <br />
+      My favorite image on this PC:
+      <br />
+      <img
+        id="wd-my-image"
+        src="/images/th.jpg"
+        height="200px"
+        alt="My current discord profile picture"
+      />
+    </div>
+  );
+}
